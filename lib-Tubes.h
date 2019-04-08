89f9d0ecdd3c5a1bc77109b6ea8f32542a569f10
Christofel Ganteng <47190511+christofel04@users.noreply.github.com>
2019-04-08 14:39:06 +0700

    Update lib-Tubes.h

--- lib-Tubes.h
+++ lib-Tubes.h
@@ -3,37 +3,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Dbs_Day_PrakT{ //Tipe data untuk memuat data praktikum pada suatu hari
-    char LAB1[10];
-    char LAB2[10];
-    char LAB3[10];
-    char LSS[10];
-    
-} Dbs_Day_PrakT;
-
-typedef struct Dbs_Day_AsistenT{ //Tipe data untuk memuat data asisten pada suatu hari
-    char LAB1[4];
-    char LAB2[4];
-    char LAB3[4];
-    char LSS[4];
-    
-} Dbs_Day_AsistenT;
 
 typedef struct Dbs_Week_PrakT{ //Tipe data untuk memuat data praktikum pada suatu minggu
-    Dbs_Day_PrakT Senin;
-    Dbs_Day_PrakT Selasa;
-    Dbs_Day_PrakT Rabu;
-    Dbs_Day_PrakT Kamis;
-    Dbs_Day_PrakT Jumat;
+    char Senin[4][10];
+    char Selasa[4][10];
+    char Rabu[4][10];
+    char Kamis[4][10];
+    char Jumat[4][10];
 
 } Dbs_Week_PrakT;
 
 typedef struct Dbs_Week_AsistenT{ //Tipe data untuk menyimpan data asisten pada suatu minggu
-    Dbs_Day_AsistenT Senin;
-    Dbs_Day_AsistenT Selasa;
-    Dbs_Day_AsistenT Rabu;
-    Dbs_Day_AsistenT Kamis;
-    Dbs_Day_AsistenTr Jumat;
+    char Senin[4][4];
+    char Selasa[4][4];
+    char Rabu[4][4];
+    char Kamis[4][4];
+    char Jumat[4][4];
 
 } Dbs_Week_AsistenT;
 
