be7e5d77ab37d8a5107cb65704704a8e0303edc6
Christofel Ganteng <47190511+christofel04@users.noreply.github.com>
2019-04-12 08:24:20 +0700

    Update Save_File.c

--- Save_File.c
+++ Save_File.c
@@ -2,11 +2,23 @@
 
 
 
+
+
+
+
 //                      [ Source Code ]
 
 
 
+
+
+
+
 //
+
+
+
+
 
 
 
@@ -14,7 +26,15 @@
 
 
 
+
+
+
+
 // Name: A2
+
+
+
+
 
 
 
@@ -22,11 +42,23 @@
 
 
 
+
+
+
+
 // Dependency : stdio.h, stdlib.h, string.h, lib-Tubes.h
 
 
 
+
+
+
+
 //
+
+
+
+
 
 
 
@@ -34,7 +66,15 @@
 
 
 
+
+
+
+
 // Library untuk database menyimpan schedule praktikum, schedule asisten, dan daftar hari libur
+
+
+
+
 
 
 
@@ -42,7 +82,15 @@
 
 
 
+
+
+
+
 // Status:
+
+
+
+
 
 
 
@@ -50,7 +98,15 @@
 
 
 
+
+
+
+
 // 2. <Nama Praktikan> - <NIM> : Fix the bug where number 2 printed twice
+
+
+
+
 
 
 
@@ -62,118 +118,243 @@
 
 
 
+
+
+
+
+
+
+
+
 //BELOM LENGKAP: -
+
+
 
 //-------------------------------------------------
 
+
+
 #include "lib-Tubes.h"
+
 #include<stdio.h>
+
 #include<String.h>
+
 #include<stdlib.h>
+
 #include<stdbool.h>
+
+
 
 bool saveProject(Dbs_Type Dbs){
 
+
+
     char File_Name[100], Day;
+
     FILE *pFile;
 
+
+
     printf("\nTentukan di file mana akan disimpan schedule praktikum (.csv) :");
+
     scanf("%s", File_Name);
+
+
 
     pFile= fopen(File_Name, "w+");
 
+
+
     if(pFile){ //Jika file tidak dapat dibuka
+
         printf("\nFile tidak dapat ditemukan\nApakah akan masukkan lagi?(Y T)"); //Cek apakah ingin mengulangi atau tidak
+
         char c;
+
         scanf("%c", c );
+
         if ((c=='Y')||(c=='y')){return(false);} else{return(true);}
+
     }
+
     else{ //Jika file dapat dibuka
 
+
+
         //Tuliskan header
+
         fprintf(pFile, "Ruang Praktikum;Lab1:LAB2;LAB3;LSS\nMinggu;Hari\n");
 
+
+
         //Tuliskan isi database
+
         for (int i=3; i<= 14; i++){
+
             for (int j=1; j<=5; j++){
+
                 switch(j){
+
                     case 1: strcpy(Day,"Senin"); break;
+
                     case 2: strcpy(Day,"Selasa"); break;
+
                     case 3: strcpy(Day,"Rabu"); break;
+
                     case 4: strcpy(Day,"Kamis"); break;
+
                     case 5: strcpy(Day,"Jumat"); break;
+
+
 
                 }
 
+
+
                /* fprintf(pFile, "%d;%s;%s-%s;%s-%s;%s-%s;%s-%s\n", i, Day, Dbs.Prak.(Day)[1].Kode, Dbs.Prak.(Day)[1].Romb,
+
                          Dbs.Prak.(Day)[2].Kode,Dbs.Prak.(Day)[2].Romb, Dbs.Prak.(Day)[3].Kode,Dbs.Prak.(Day)[3].Romb,
+
                          Dbs.Prak.(Day)[4].Kode, Dbs.Prak.(Day)[4].Romb);
 
+
+
                 */
+
         }
+
+
+
 
 
         fclose(pFile);
 
+
+
         //Save file asisten
 
+
+
         printf("\nTentukan di file mana akan disimpan schedule asisten (.csv) :");
+
         scanf("%s", File_Name);
+
+
 
         pFile= fopen(File_Name, "w+");
 
+
+
         if(pFile){ //Jika file tidak dapat dibuka
+
             printf("\nFile tidak dapat ditemukan\nApakah akan masukkan lagi?(Y T)"); //Cek apakah ingin mengulangi atau tidak
+
             char c;
+
             scanf("%c", c );
+
             if ((c=='Y')||(c=='y')){return(false);} else{return(true);}
+
         }
+
         else{ //Jika file dapat dibuka
 
+
+
             //Tuliskan header
+
             fprintf(pFile, "Ruang Praktikum;Lab1:LAB2;LAB3;LSS\nMinggu;Hari\n");
 
+
+
             //Tuliskan isi database
+
             for (int i=3; i<= 14; i++){
+
                 for (int j=1; j<=5; j++){
+
                     switch(j){
+
                         case 1: strcpy(Day,"Senin"); break;
+
                         case 2: strcpy(Day,"Selasa"); break;
+
                         case 3: strcpy(Day,"Rabu"); break;
+
                         case 4: strcpy(Day,"Kamis"); break;
+
                         case 5: strcpy(Day,"Jumat"); break;
 
-                    }}
-                
+
+
+                    }
                     switch(j){
-                        case 1:    fprintf(pFile, "%d;%s;%s;%s;%s;%s\n", i, Day, Dbs.Asist.(Senin)[1], Dbs.Asist.(Senin)[2]
-                            Dbs.Asist.(Senin)[3], Dbs.Asist.(Senin)[4]);
+
+                        case 1:    fprintf(pFile, "%d;%s;%s;%s;%s;%s\n", i, Day, Dbs.Asist[i].Senin[1], Dbs.Asist[i].Senin[2],
+
+                            Dbs.Asist[i].Senin[3], Dbs.Asist[i].Senin[4]);
+
                             break;
-                        
-                         case 2:    fprintf(pFile, "%d;%s;%s;%s;%s;%s\n", i, Day, Dbs.Asist.(Selasa)[1], Dbs.Asist.(Selasa)[2]
-                            Dbs.Asist.(Selasa)[3], Dbs.Asist.(Selasa)[4]);
+
+
+
+                         case 2:    fprintf(pFile, "%d;%s;%s;%s;%s;%s\n", i, Day, Dbs.Asist[i].Selasa[1], Dbs.Asist[i].Selasa[2],
+
+                            Dbs.Asist[i].Selasa[3], Dbs.Asist[i].Selasa[4]);
+
                             break;
-                            
-                         case 3:    fprintf(pFile, "%d;%s;%s;%s;%s;%s\n", i, Day, Dbs.Asist.(Rabu)[1], Dbs.Asist.(Rabu)[2]
-                            Dbs.Asist.(Rabu)[3], Dbs.Asist.(Rabu)[4]);
+
+
+
+                         case 3:    fprintf(pFile, "%d;%s;%s;%s;%s;%s\n", i, Day, Dbs.Asist[i].Rabu[1], Dbs.Asist[i].Rabu[2],
+
+                            Dbs.Asist[i].Rabu[3], Dbs.Asist[i].Rabu[4]);
+
                             break;
-                            
-                          case 4:    fprintf(pFile, "%d;%s;%s;%s;%s;%s\n", i, Day, Dbs.Asist.(Kamis)[1], Dbs.Asist.(Kamis)[2]
-                            Dbs.Asist.(Kamis)[3], Dbs.Asist.(Kamis)[4]);
+
+
+
+                          case 4:    fprintf(pFile, "%d;%s;%s;%s;%s;%s\n", i, Day, Dbs.Asist[i].Kamis[1], Dbs.Asist[i].Kamis[2],
+
+                            Dbs.Asist[i].Kamis[3], Dbs.Asist[i].Kamis[4]);
+
                             break;
-                            
-                            
-                           case 5:    fprintf(pFile, "%d;%s;%s;%s;%s;%s\n", i, Day, Dbs.Asist.(Jumat)[1], Dbs.Asist.(Jumat)[2]
-                            Dbs.Asist.(Jumat)[3], Dbs.Asist.(Jumat)[4]);
+
+
+
+
+
+                           case 5:    fprintf(pFile, "%d;%s;%s;%s;%s;%s\n", i, Day, Dbs.Asist[i].Jumat[1], Dbs.Asist[i].Jumat[2],
+
+                            Dbs.Asist[i].Jumat[3], Dbs.Asist[i].Jumat[4]);
+
                             break;
-                        
-                            
-                            
+
+
+
+
+
+
+
                     }
 
+
+
+                    }
+
+
+
+
+
+
                  /*   fprintf(pFile, "%d;%s;%s;%s;%s;%s\n", i, Day, Dbs.Asist.(Day)[1], Dbs.Asist.(Day)[2]
+
                             Dbs.Asist.(Day)[3], Dbs.Asist.(Day)[4]);
+
                 */
+
+
 
             }
 
@@ -181,13 +362,30 @@ bool saveProject(Dbs_Type Dbs){
 
 
 
+
+
+
+
+
+
             fclose(pFile);
+
         return(true);
+
+    }
+
+
+
+
+
+
+
     }
 
 
 
-    }
 
 
 }}
+
+
