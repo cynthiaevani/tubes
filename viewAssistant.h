ad4e18a99c1f84553320e7c0804eb83a1f742bdc
Cynthia Evani <49182111+cynthiaevani@users.noreply.github.com>
2019-04-12 08:22:33 +0700

    Create viewAssistant.h

---
+++ viewAssistant.h
@@ -0,0 +1,26 @@
+//***********************************************************//
+//                      [ Source Code ]
+//
+// Institution: Institut Teknologi Bandung
+// Name: A2/4
+// File Name: view_assistant.h
+// Dependency : stdio.h, stdlib.h, string.h, LIB-TUBES.h
+//
+// Description:
+// Header untuk view assistant.
+//
+// Status:
+// 1. Cynthia Evani - 13217045 : Buat header viewAssistant
+//***********************************************************//
+
+#ifndef view_assistant
+#define view_assistant
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "LIB-TUBES.h"
+
+struct Dbs tabelOut;
+void View_Assistant();
+
+#endif
