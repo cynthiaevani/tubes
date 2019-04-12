cac32458a943bd6236af499d7dd32a323715094e
Cynthia Evani <49182111+cynthiaevani@users.noreply.github.com>
2019-04-12 08:23:34 +0700

    Update and rename viewSchedule.h to view.h

--- view.h
+++ view.h
@@ -3,14 +3,14 @@
 //
 // Institution: Institut Teknologi Bandung
 // Name: A2/4
-// File Name: viewSchedule.h
+// File Name: view.h
 // Dependency : stdio.h, stdlib.h, string.h, LIB-TUBES.h
 //
 // Description:
-// Header untuk view schedule.
+// Header untuk view.
 //
 // Status:
-// 1. Cynthia Evani - 13217045 : Buat header viewSchedule
+// 1. Cynthia Evani - 13217045 : Buat header view
 //***********************************************************//
 #ifndef view_schedule
 #define view_schedule
@@ -21,5 +21,6 @@
 
 struct Dbs tabelOut;
 void View_Schedule();
+void View_Assistant();
 
 #endif
