df5e37ddbce932d79283118b59eee45a7a82e389
Cynthia Evani <49182111+cynthiaevani@users.noreply.github.com>
2019-04-12 08:12:52 +0700

    Create manual_schedule.h

---
+++ manual_schedule.h
@@ -0,0 +1,29 @@
+//***********************************************************//
+//                      [ Source Code ]
+//
+// Institution: Institut Teknologi Bandung
+// Name: A2/4
+// File Name: manual_schedule.h
+// Dependency : stdio.h, stdlib.h, string.h, LIB-TUBES.h
+//
+// Description:
+// Header untuk manual schedule.
+//
+// Status:
+// 1. Cynthia Evani - 13217045 : Buat header manual schedule
+//***********************************************************//
+
+#ifndef manual_schedule
+#define manual_schedule
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "LIB-TUBES.h"
+
+char kodeprak[10], asst[10];
+struct inputpraktikum assign;
+struct Dbs tabelOut;
+int j;
+int cekquit = 0;
+
+#endif
