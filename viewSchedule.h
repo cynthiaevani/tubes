0ca0c8699f4949796e3820e038a6222376af49ef
Cynthia Evani <49182111+cynthiaevani@users.noreply.github.com>
2019-04-12 08:17:37 +0700

    Create viewSchedule.h

---
+++ viewSchedule.h
@@ -0,0 +1,25 @@
+//***********************************************************//
+//                      [ Source Code ]
+//
+// Institution: Institut Teknologi Bandung
+// Name: A2/4
+// File Name: manual_schedule.h
+// Dependency : stdio.h, stdlib.h, string.h, LIB-TUBES.h
+//
+// Description:
+// Header untuk manual schedule.
+//
+// Status:
+// 1. Cynthia Evani - 13217045 : Buat header viewSchedule
+//***********************************************************//
+#ifndef view_schedule
+#define view_schedule
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "LIB-TUBES.h"
+
+struct Dbs tabelOut;
+void View_Schedule();
+
+#endif
